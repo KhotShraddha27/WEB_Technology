d308f5081eedb259edb5a751b47ebe4bfb66aff6
Shraddha Khot <khotshraddha27@gmail.com>
2026-01-20 10:45:15 +0530

    Day03-20/01/2026

---
+++ Class/String.js
@@ -0,0 +1,18 @@
+let str1= "Hari";
+console.log(str1);
+console.log(typeof(str1));
+
+//String interpolation
+console.log(`My name is :${str1}`);
+
+let fname = "Ram";
+console.log(`${fname}`);
+
+const gCount= new String("Hari");//String itself is an object 
+console.log(gCount.length);
+console.log(gCount.toLowerCase());
+console.log(gCount.toUpperCase());
+console.log(gCount.charAt(2));
+console.log(gCount.indexOf('i'));
+
+//Activity02--all methods 
---
+++ Class/dataTypeSummary.js
@@ -0,0 +1,30 @@
+// Non-Primitive 
+// object 
+// Array
+// Function
+
+//Symbol example--used for unique Id
+const Id1 = Symbol ("123");
+console.log(typeof(Id1));
+
+const Id = 123;
+console.log(Id===Id1);
+
+//Array
+const Hero = ["Hari","Narayan","Rama"];
+const num = [1,2,3];
+
+//Objects--key:value pair
+const myObj ={
+    fName : "JS",
+    age : 24
+}
+console.log(myObj);
+
+//Functions-- special block of code written for performing specific task 
+//Activity 01--deep study of function
+const  myfun = function sum (){
+    
+}
+
+//memory related story --stack and heap
