fdc0e07b536fcd2fc8f2f3f80306a05a81a584d8
Shraddha Khot <khotshraddha27@gmail.com>
2026-02-01 16:24:18 +0530

    LAB-expt_01

--- Lab/Expt_01.js
+++ Lab/Expt_01.js
@@ -1,93 +1,105 @@
 // -- let var const -- Activity 01
+
 {
     //varibles declared with let can't be redeclared
     let x= 2;// Provides Block Scope in js
-    console.log(x);      
-    document.writeln(x);
+    console.log(`This is a let variable x = ${x}`);      
+    document.writeln(`This is a let variable x = ${x}<br>`);  
 
 }
 
 {
-   //varibles declared with var can't be redeclare
+   //varibles declared with var CAN be redeclared
     var x= 2;
-    console.log(x);
-    document.writeln(x);
+    console.log(`This is a var variable x = ${x}`);  
+    document.writeln(`This is a var variable x = ${x}<br>`);  
 
 }
 
 {
-    //variable defined with const cant be reassigned.
+    //variable defined with const CAN'T be reassigned.
     const PI = 3.1415;
-    //PI =3.14;    
-    console.log(PI);
-    document.writeln(PI);
+    //PI =3.14;    //gives an error
+    console.log(`This is a const variable PI = ${PI}.`);  
+    document.writeln(`This is a const variable PI = ${PI}.<br>`);
 }
 
 //Block of code --Activity 02
 let a=2;
 {
     let a=10;
+    console.log(`This value ${a} of a is inside the block of code.`);
+    document.writeln(`This value ${a} of a is inside the block of code.<br>`);
 }
-console.log(a);
-document.writeln(a);
+console.log(`This value ${a} of a is outside the block of code.`);
+document.writeln(`This value ${a} of a is outside the block of code.<br>`);
 
 var b=20;
 {
     var b =30;
+    console.log(`This value ${b} of b is inside the block of code.`);
+    document.writeln(`This value ${b} of b is inside the block of code.<br>`);
 }
-console.log(b);
-document.writeln(b);
+console.log(`This value ${b} of b is outside the block of code.`);
+document.writeln(`This value ${b} of b is outside the block of code.<br>`);
 
 const c=40;
 {
     const c=60;
+    console.log(`This value ${c} of c is inside the block of code.`);
+    document.writeln(`This value ${c} of c is inside the block of code.<br>`);
 }
-console.log(c);
+console.log(`This value ${c} of c is outside the block of code.`);
+document.writeln(`This value ${c} of c is outside the block of code.<br>`);
 
-//--Display student information-- Activity 04
-console.log("Display student information");
-let firstName ="Hari";
-let lastName ="Narayan";
-let age = 5;
-let collegeName= "DKTE";
+//--Display student information-- Activity 03
+console.log("Display student information : ");
+let fName ="Shraddha";
+let lName ="Khot";
+let age = 19;
+let cName= "DKTE";
 let result = true;
 let cgpa = 8.5;
 
-console.log({firstName});
-document.write({firstName});
-console.log({lastName});
-document.write({lastName});
-console.log({age});
-document.write({age});
-console.log({collegeName});
-document.write({collegeName});
-console.log({result});
-document.write({result});
-console.log({cgpa});
-document.write({cgpa});
+console.log(`FirstName : ${fName}`);
+document.writeln(`FirstName : ${fName}<br>`);
+console.log(`LastName : ${lName}`);
+document.writeln(`LastName : ${lName}<br>`);
+console.log(`Age : ${age}`)
+document.writeln(`Age : ${age}<br>`)
+console.log(`College Name : ${cName}`);
+document.writeln(`College Name : ${cName}<br>`);
+console.log(`Result :${result}`)
+document.writeln(`Result :${result}<br> `)
+console.log(`CGPA : ${cgpa}`);
+document.writeln(`CGPA : ${cgpa}<br>`)
 
-// -- Check number is even/odd -- Activity 03
-console.log("Check if num is even or odd");
+// -- Check number is even/odd -- Activity 04
+document.writeln("Check if num is even or odd : ");
 var no = 56;
 if (no%2==0){
-    //console.log("Number is even.");
-    document.writeln("Number is even.");
+    console.log(`${no} is even.`)
+    document.writeln(`${no} is even.<br>`)
 }
 else{
-    //console.log("Number is odd.");
-    document.writeln("Number is odd.")
+    console.log("Number is odd.");
+    document.writeln("Number is odd.<br>");
 }
 
-//--Check student is pass or fail --Activity 04
+//--Check student is pass or fail --Activity 05
 const marks =35;
 if (marks>=50){
     console.log("Student is passed!!");
+    document.writeln("Studetn is passed!<br>");
 }
 else{
     console.log("Student is failed!!");
+    document.writeln("Student is failed!!<br>");
 }
-//--Display numbers upto 50
-for(i=0;i<=50;i++){
-    console.log("Numbers froms 0 to 50:")
+//--Display numbers upto 50 -- Activity 05
+console.log("Numbers froms 0 to 10: ")
+document.writeln("Numbers froms 0 to 10: ")
+for(i=0;i<=10;i++){
     console.log(i);
+    document.writeln(i)
 }
